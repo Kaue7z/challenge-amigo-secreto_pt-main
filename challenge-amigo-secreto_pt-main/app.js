ae027bac24ebb025198959e70de89bd42d39e9ef
Kaue <kauemata24@gmail.com>
2025-09-06 01:28:59 -0300

    criando Funcao sortear amigo

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,40 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function capturaAmigo (){
+    let campo = document.getElementById("amigo")
+    let nome = campo.value.trim()
+    
+    if(nome === ""){
+        alert("digite um nome")
+        return;
+    } 
+    amigos.push(nome)
+    atualizarLista();
+    campo.value = ""
+}
+
+function atualizarLista (amigos) {
+    let listaAmigos = document.getElementById("listaAmigos")
+    listaAmigos.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++){
+        let item = document.createElement("li")
+        item.textContent = amigos[i]
+        listaAmigos.appendChild(item)
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert("A lista esta vazia!");
+        return
+    }
+
+    let indice = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indice];
+    console.log(amigoSorteado)
+
+    document.getElementById("resultado").innerHTML = `O amigo sorteado foi ${amigoSorteado}`
+}
+
